d64c5ebeb76539b944217b98f89392121a382ed0
Xiomara7 <figueroaxiomara7@gmail.com>
2014-10-16 13:36:47 -0400

    more code

--- memory/optimal.py
+++ memory/optimal.py
@@ -27,7 +27,7 @@ class pages:
 	
 '''
 	Predefine variables: 
-		PM_pages: Size of the PM
+		PM_pages: Size of the physical memory
 		seq_file: Input file
 		Q_pages:  PM of type 'pages'
 		Q_value:  queue to manage just the values of the pages
@@ -66,56 +66,77 @@ def getIndex(queue, elem):
 		if q == elem: 
 			return i
 		i += 1
-
+'''
+Function to be call everytime an instruction
+needs to be write to the disk [ex - W:3] 
+'''
 def writeToDisk():
 	print 'Write to disk'
 
 for v in file_content:		# for each element in the input file 
-	value = v.split(':')
 	item  = pages() 		# item of type 'pages'
+	value = v.split(':')
 	item.value = value[1]
-	# while the elements in the PM < size of the PM
+	# If the elements in the PM < size of the PM
 	if len(Q_pages) < PM_pages:
 		if item.value in Q_value:
-			# If the value is already in, just reference it 
+			# If the value is already in, get the index 
+			# and reference the element in that position 
 			index_value = getIndex(Q_value, item.value)
 			Q_pages[index_value].refer = 1
+			# If it's a write instruction, write it to 
+			# the disk and mark it as modified
 			if value[0] == 'W':
 				writeToDisk()
 				Q_pages[index_value].modif = 1
 		else:
-			# If not, added to the list and increment the page faults
-			pfaults += 1
+			# If it's not in the queue, added to the list 
+			# and increment the page faults
 			Q_pages.append(item)
 			Q_value.append(item.value)
+			pfaults += 1
+	# If the queue is already full
 	elif len(Q_pages) == PM_pages:
-		# If the queue is already full
+		# If the value is already in, get the index 
+		# and reference the element in that position 
 		if item.value in Q_value: 
-			# while the elements in the PM < size of the PM
 			index_value = getIndex(Q_value, item.value)
 			Q_pages[index_value].refer = 1
+			# If it's a write instruction, write it to 
+			# the disk and mark it as modified
 			if value[0] == 'W':
 				writeToDisk()
 				Q_pages[index_value].modif = 1
+		# If not, increment the page faults and look in 
+		# the rest of the list, the farthest element
 		else:
-			# If not, look in the rest of the list, the farthest element
 			pfaults += 1
 			farthest_elem  = 0
 			farthest_index = 0
 			for page in Q_value:
-				rest = file_content[index_content:len(file_content)]	# Rest of the original list
+				# Rest of the original list
+				rest = file_content[index_content:len(file_content)]	
+				# If the page will not be referenced anymore, [It 
+				# doesn't appear again] claimed it
 				if page not in rest:
 					farthest_elem = page
 					break 
+				# If it appears again in the rest of the list
+				# look for the farthest one and insert the 
+				# new item there
 				elif page in rest:
 					index_value = getIndex(rest, page)
 					if index_value > farthest_index: 
 						farthest_elem  = page
 						farthest_index = index_value
+			# Find the index that corresponds to the farthest 
+			# element in the PM queue and replace it qith the 
+			# new element
 			index = getIndex(Q_value, farthest_elem)
 			Q_pages[index] = item
 			Q_value[index] = item.value
 	index_content += 1
+	# Print the actual state of the pm
 	for q in Q_pages: 
 		print q.value
 	print '________________________'
